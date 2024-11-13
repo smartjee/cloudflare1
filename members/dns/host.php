daca3d1232db457994041188b246305433285c72
smartjee <smartje84@hotmail.com>
2024-11-13 02:46:03 +0100

    Update host.php

--- members/dns/host.php
+++ members/dns/host.php
@@ -1,1 +1,9 @@
-
+<?php
+  $fh = fopen("prueba.txt") or die("Se produjo un error al crear el archivo");
+  
+  fwrite($fh) or die("No se pudo escribir en el archivo");
+  
+  fclose($fh);
+  
+  echo "Se ha escrito sin problemas";
+?>
